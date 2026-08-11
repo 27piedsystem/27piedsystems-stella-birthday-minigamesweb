e50d85e3c7a42531f6a0a72970e2ca9511a1639e
Keith A Johnson <ethxvr27@gmail.com>
2026-08-11 11:59:50 +0700

    Update click event for startAdventureButton
    Signed-off-by: Keith A Johnson <ethxvr27@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const startButton = document.getElementById("startAdventureButton");
 
-  if (startButton) {
-    startButton.addEventListener("click", function () {
-      alert("The Birthday Adventure is starting!");
-    });
-  }
+  const button = document.getElementById("startAdventureButton");
+
+  button.addEventListener("click", function () {
+
+    alert("YES! JAVASCRIPT IS WORKING!");
+
+  });
+
 });
